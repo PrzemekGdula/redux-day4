73b9ee0c908e34a86318bef84fcc475217398c23
PrzemekGdula <przemo.g12@wp.pl>
2019-04-18 14:51:32 +0200

    const user id fixed

--- src/state/auth.js
+++ src/state/auth.js
@@ -6,128 +6,130 @@ const SET_USER = 'auth/SET_USER'
 const SET_USER_LOGINS_LOGS = 'auth/SET_USER_LOGINS_LOGS'
 
 export const startListeningToAuthChangesAsyncActionCreator = (
-  () => (dispatch, getState) => {
-    auth.onAuthStateChanged(
-      (user) => {
-        console.log(user)
+    () => (dispatch, getState) => {
+        auth.onAuthStateChanged(
+            (user) => {
+                console.log(user)
 
-        if (user) {
-          // USER LOGGED IN
-          dispatch(setUserActionCreator(user))
-          dispatch(passChangedActionCreator(''))
-          dispatch(emailChangedActionCreator(''))
-          dispatch(logUserLoginsAsyncActionCreator())
-          dispatch(startListeningUserLoginsLogsAsyncActionCreator())
-        } else {
-          // USER NOT LOGGED IN
-          dispatch(stopListeningUserLoginsLogsAsyncActionCreator())
-          dispatch(setUserLoginsLogsActionCreator(null))
-          dispatch(setUserActionCreator(user))
-        }
-      }
-    )
-  }
+                if (user) {
+                    // USER LOGGED IN
+                    dispatch(setUserActionCreator(user))
+                    dispatch(passChangedActionCreator(''))
+                    dispatch(emailChangedActionCreator(''))
+                    dispatch(logUserLoginsAsyncActionCreator())
+                    dispatch(startListeningUserLoginsLogsAsyncActionCreator())
+                } else {
+                    // USER NOT LOGGED IN
+                    dispatch(stopListeningUserLoginsLogsAsyncActionCreator())
+                    dispatch(setUserLoginsLogsActionCreator(null))
+                    dispatch(setUserActionCreator(user))
+                }
+            }
+        )
+    }
 )
 export const stopListeningUserLoginsLogsAsyncActionCreator = (
-  () => (dispatch, getState) => {
-    const state = getState()
-    const userId = state.auth.user.uid
-  
-    database.ref(`users/${userId}/login`).off()
-  }
+    () => (dispatch, getState) => {
+        const state = getState()
+        const userId = state.auth.user.uid && state.auth.user.uid
+
+        if (!userId) return
+
+        database.ref(`users/${userId}/login`).off()
+    }
 )
 export const startListeningUserLoginsLogsAsyncActionCreator = (
-  () => (dispatch, getState) => {
-    const state = getState()
-    const userId = state.auth.user.uid
-  
-    database.ref(`users/${userId}/login`)
-      .on(
-        'value',
-        (snapshot) => {
-          dispatch(
-            setUserLoginsLogsActionCreator(
-              snapshot.val()
+    () => (dispatch, getState) => {
+        const state = getState()
+        const userId = state.auth.user.uid
+
+        database.ref(`users/${userId}/login`)
+            .on(
+                'value',
+                (snapshot) => {
+                    dispatch(
+                        setUserLoginsLogsActionCreator(
+                            snapshot.val()
+                        )
+                    )
+                }
             )
-          )
-        }
-      )
-  }
+    }
 )
 export const logUserLoginsAsyncActionCreator = () => (dispatch, getState) => {
-  const state = getState()
-  const userId = state.auth.user.uid
+    const state = getState()
+    const userId = state.auth.user.uid
 
-  database.ref(`users/${userId}/login`)
-    .push({
-      timestamp: Date.now(),
-    })
+    database.ref(`users/${userId}/login`)
+        .push({
+            timestamp: Date.now(),
+        })
 }
 export const logInAsyncActionCreator = () => (dispatch, getState) => {
-  const state = getState()
-  const email = state.auth.email
-  const password = state.auth.password
+    const state = getState()
+    const email = state.auth.email
+    const password = state.auth.password
 
-  auth.signInWithEmailAndPassword(email, password)
-    .then(() => console.log('ZALOGOWANO'))
-    .catch((error) => console.log('WYSTĄPIŁ BŁĄD', error))
+    auth.signInWithEmailAndPassword(email, password)
+        .then(() => console.log('ZALOGOWANO'))
+        .catch((error) => console.log('WYSTĄPIŁ BŁĄD', error))
 }
 export const logInByGoogleAsyncActionCreator = () => (dispatch, getState) => {
-  auth.signInWithPopup(googleProvider)
-    .then(() => console.log('ZALOGOWANO'))
-    .catch((error) => console.log('WYSTĄPIŁ BŁĄD', error))
+    auth.signInWithPopup(googleProvider)
+        .then(() => console.log('ZALOGOWANO'))
+        .catch((error) => console.log('WYSTĄPIŁ BŁĄD', error))
 }
 export const logOut = () => (dispatch, getState) => {
-  auth.signOut()
+    auth.signOut()
 }
 
 const setUserLoginsLogsActionCreator = data => ({
-  type: SET_USER_LOGINS_LOGS,
-  data
+    type: SET_USER_LOGINS_LOGS,
+    data
 })
 const setUserActionCreator = user => ({
-  type: SET_USER,
-  user,
+    type: SET_USER,
+    user,
 })
 export const emailChangedActionCreator = newValue => ({
-  type: EMAIL_CHANGED,
-  newValue,
+    type: EMAIL_CHANGED,
+    newValue,
 })
 export const passChangedActionCreator = newValue => ({
-  type: PASS_CHANGED,
-  newValue,
+    type: PASS_CHANGED,
+    newValue,
 })
 
 const initialState = {
-  userLoginsLogs: null,
-  user: null,
-  email: '',
-  password: '',
+    userLoginsLogs: null,
+    user: null,
+    email: '',
+    password: '',
 }
 
 export default (state = initialState, action) => {
-  switch (action.type) {
-    case EMAIL_CHANGED:
-      return {
-        ...state,
-        email: action.newValue,
-      }
-    case PASS_CHANGED:
-      return {
-        ...state,
-        password: action.newValue,
-      }
-    case SET_USER:
-      return {
-        ...state,
-        user: action.user,
-      }
-    case SET_USER_LOGINS_LOGS:
-      return {
-        ...state,
-        userLoginsLogs: action.data,
-      }
-    default:
-      return state
-  }
+    switch (action.type) {
+        case EMAIL_CHANGED:
+            return {
+                ...state,
+                email: action.newValue,
+            }
+        case PASS_CHANGED:
+            return {
+                ...state,
+                password: action.newValue,
+            }
+        case SET_USER:
+            return {
+                ...state,
+                user: action.user,
+            }
+        case SET_USER_LOGINS_LOGS:
+            return {
+                ...state,
+                userLoginsLogs: action.data,
+            }
+        default:
+            return state
+    }
 }
